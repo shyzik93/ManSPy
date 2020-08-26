5aa0df17895e55f2bacfda68f4bccd1bb2aec192
Konstantin Polyakov <shyzik93@mail.ru>
2020-08-26 21:46:20 +0300

    test is ready

--- manspy/NLModules/Esperanto/test_graphemathic.py
+++ manspy/NLModules/Esperanto/test_graphemathic.py
@@ -1,22 +1,33 @@
 import unittest
 
-import GraphemathicalAnalysis
+from manspy.NLModules.Esperanto import GraphemathicAnalysis
+from manspy.NLModules import ObjUnit
 
-def t(a):
-    return 3
 
 class TestGraphemathicalAnalysis(unittest.TestCase):
-    def test_list_int(self):
-        #data = {'symbol'}
-        #result = GraphemathicalAnalysis.define_type_symbol(data)
-        result = t(4)
-        self.assertEqual(result, 3)
+    def test_define_type_symbol(self):
+        word = ObjUnit.Word('montru')
+        word(0, 'type', 'letter')
+        word(1, 'type', 'letter')
+        word(2, 'type', 'letter')
+        word(3, 'type', 'letter')
+        word(4, 'type', 'letter')
+        word(5, 'type', 'letter')
 
-    def test_list_int2(self):
-        #data = {'symbol'}
-        #result = GraphemathicalAnalysis.define_type_symbol(data)
-        result = t(4)
-        self.assertEqual(result, 99)
+        word_result = ObjUnit.Word('montru')
+        result = GraphemathicAnalysis.define_type_symbol(word_result)
+
+        message = 'define_type_symbol: "{}" must be "{}", not "{}"'
+
+        for symbol_index, symbol in word:
+            type_ethalon = word(symbol_index, 'type')
+            type_result = word_result(symbol_index, 'type')
+            self.assertEqual(
+                type_ethalon,
+                type_result,
+                message.format(symbol, type_ethalon, type_result)
+            )
+            print(symbol, type_ethalon, type_result)
 
 
 if __name__ == '__main__':
