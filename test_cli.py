9b6459f2cc427ccab4ae79c32fca3fa0d83ddd92
shyzik93 <shyzik93@mail.ru>
2020-10-15 23:27:10 +0300

    Test for cli was fixed

--- test_cli.py
+++ test_cli.py
@@ -1,12 +1,13 @@
 import unittest
-import sys
 
 from unittest.mock import patch
 
 from manspy.cli import do_cmd
 
 TEST_ARG_LISTS = [
-    (['--text', 'montru dolran kurzon'], [])
+    ([], []),
+    (['--text', 'montru dolaran kurzon'], ['60']),
+    (['--text', 'montru adreson de komputilo kaj dolaran kurzon'], ['192.168.0.1', '60']),
 ]
 
 outputs = []
@@ -20,16 +21,8 @@ class TestCLI(unittest.TestCase):
 
     @patch('manspy.cli.print', mock_print)
     def test_cli(self):
-        def stdout(output):
-            print('out:', output)
-
-        source_stdout = sys.stdout
 
         for args_list, answers in TEST_ARG_LISTS:
-            #sys.stdout = stdout
             do_cmd(args_list)
-            print(outputs)
-            #sys.stdout = source_stdout
-            #self.assertEqual(True, False)
-
-
+            self.assertListEqual(outputs, answers, ' '.join(args_list))
+            outputs.clear()
