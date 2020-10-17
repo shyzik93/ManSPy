7d575d046a9740fb4a75f858871aa3550880a4ba
shyzik93 <shyzik93@mail.ru>
2020-10-17 15:01:45 +0300

    Code style was fixed

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
 import manspy
 import ifmodules
 
-interfaces = { # 1 - on, 0 - off
-  'autofeed':    1, # Автоподатчик предложений - для теста, но можно писать "скрипты"
+interfaces = {  # 1 - on, 0 - off
+  'autofeed':    1,  # Автоподатчик предложений - для теста, но можно писать "скрипты"
   'TKinter':     0,
   'jabber':      0,
-  'vkcom':       0, # с ошибками
+  'vkcom':       0,  # с ошибками
   'Commandline': 0,  # не имеет смысла
   'telegram': 1,
   }
