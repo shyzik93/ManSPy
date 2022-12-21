e98bc22d9c9577babe0c607f5a2064646bbca46d
shyzik93 <shyzik93@mail.ru>
2022-12-22 00:46:40 +0300

    refactoring

--- manspy/analyzers/utils.py
+++ manspy/analyzers/utils.py
@@ -1,4 +1,96 @@
+import itertools
+
 from manspy.storage.fasif.finder import find
+
+not_to_db = ['nombr', 'cifer']  # TODO:  существует ещё одна переменная с таким же именем. Нужно сделать общий источник
+
+
+def dproduct(dparentl):
+    """
+    Выполняет декартово (прямое) произведение над словарями в списке dparent1
+    В стандартной библиотеке Python я этой функции не нашёл.
+    Функция необходима для аргументных слов.
+    :param dparentl: {'a': [1,2], 'b': [5,6]}
+    :return: [{'a': 1, 'b':5}, {'a': 1, 'b':6}, {'a': 2, 'b':5}, {'a': 2, 'b':6}]
+    """
+    def _dproduct(resl, values, key):
+        """
+        [
+            (('a',1), ('b',5)),
+            (('a',1), ('b',6)),
+            (('a',2), ('b',5)),
+            (('a',2), ('b',6))
+        ]
+        """
+        n = len(values)
+        _resl = list(resl)
+        for i, el in enumerate(resl, 0):
+            for j in range(n - 1):
+                _resl.insert(i * n, dict(el))
+            for j in range(n):
+                _resl[j + n * i][key] = values[j]
+        return _resl
+    prevchildl = [{}]
+    for key, values in dparentl.items():
+        prevchildl = _dproduct(prevchildl, values, key)
+    return prevchildl
+
+
+def dproduct2(dparent1):
+    """ Второй вариант функции. TODO: Необходимо обе протестировать на скорость """
+    l = [([k], v) for k, v in dparent1.items()]
+    l = [[i for i in itertools.product(*subl)] for subl in l]
+    l = [i for i in itertools.product(*l)]
+    l = [dict(i) for i in l]
+    return l
+
+
+def is_in_hyperonym(hyperonyms, argvalue, relation):
+    for hyperonym in hyperonyms:
+        if (hyperonym in not_to_db and isinstance(argvalue, (int, float, complex))) or \
+                relation.isRelBetween('hyperonym', hyperonym, argvalue):
+            return True
+
+    return False
+
+
+def convert_by_argtable(argdescr, argname, argvalue):
+    if argvalue not in argdescr[argname]['argtable']:
+        return argvalue
+
+    return argdescr[argname]['argtable'][argvalue]
+
+
+def check_args(finded_args, fasif, relation, language):
+    # Проверка на наличие в абстрактной группе
+    hyperonyms = {}
+    for argname, data in fasif['argdescr'][language].items():
+        # пока только основные гиперонимы вытягиваем
+        hyperonyms[argname] = [word['base'] for word in data['hyperonyms']]
+    for finded_arg in finded_args:
+        for argname, argvalue in list(finded_arg.items()):
+            if not is_in_hyperonym(hyperonyms[argname], argvalue, relation):
+                del finded_arg[argname]
+
+    # Проверка на отсутствие обязательных аргументных слов
+    checked_args = []
+    for finded_arg in finded_args:
+        isright = True
+        for argname, argdescr in fasif['argdescr'][language].items():
+            if argname not in finded_arg and argdescr['isreq']:  # если отсутствует обязательный аргумент
+                isright = False
+                break
+
+        if isright:
+            checked_args.append(finded_arg)
+
+    # Конвертирование аргументных слов по таблице из фасифа
+    for checked_arg in checked_args:
+        for argname, argvalue in checked_arg.items():
+            # TODO: раскрыть функцию convert_by_argtable
+            checked_arg[argname] = convert_by_argtable(fasif['argdescr'][language], argname, argvalue)
+
+    return checked_args
 
 
 def get_func_common(relation, verb_id_group, settings):
@@ -37,10 +129,12 @@ def il_build_func_value(data_func, language, verb_id_group=None, check_verb=Fals
     return False
 
 
-def get_func_wcomb_for_arguments(argument, settings, verb_id_group):
+def get_func_wcomb_for_arguments(argument, settings, verb_id_group, relation):
     compared_fasifs = find(settings, 'word_combination', argument, settings.language)
     if compared_fasifs:
         finded_args, fasif = compared_fasifs[0]  # если фасифов несколько, то необходимо отсеть лишние в этом месте (отдельной функцией)
+        finded_args = dproduct(finded_args)
+        finded_args = check_args(finded_args, fasif, relation, settings.language)
 
         # Вынимаем функцию получения/изменения состояния.
 
@@ -48,20 +142,30 @@ def get_func_wcomb_for_arguments(argument, settings, verb_id_group):
         finded_get_by_antonym = il_build_func_value(data_get_value, settings.language)
         data_set_value = fasif['functions'].get('changeCondition')
         finded_set_by_antonym = il_build_func_value(data_set_value, settings.language, verb_id_group, check_verb=True)
-        return data_get_value, data_set_value, finded_args, fasif, finded_set_by_antonym
+        return (
+            data_get_value['function'] if data_get_value else None,
+            data_set_value['function'] if data_set_value else None,
+            finded_args,
+            finded_set_by_antonym,
+        )
 
-    return None, None, None, None, None
+    return None, None, None, None
 
 
-def get_func_wcomb_for_subjects(subject, settings):
+def get_func_wcomb_for_subjects(subject, settings, relation):
     compared_fasifs = find(settings, 'word_combination', subject, settings.language)
     if compared_fasifs:
         finded_args, fasif = compared_fasifs[0]  # если фасифов несколько, то необходимо отсеть лишние в этом месте (отдельной функцией)
+        finded_args = dproduct(finded_args)
+        finded_args = check_args(finded_args, fasif, relation, settings.language)
 
         # Вынимаем функцию получения состояния.
 
         data_get_value = fasif['functions'].get('getCondition')
-        _ = il_build_func_value(data_get_value, settings.language)
-        return data_get_value, finded_args, fasif
+        finded_get_by_antonym = il_build_func_value(data_get_value, settings.language)
+        return (
+            data_get_value['function'] if data_get_value else None,
+            finded_args,
+        )
 
-    return None, None, None
+    return None, None
